5b60fbc00051925948b1d4d71153e7eae27fb1d0
usases <igor.sergeev.05@list.ru>
2025-01-08 15:50:33 +0300

    -

---
+++ src/main/java/net/usases/kordsmod/KordsModClient.java
@@ -0,0 +1,10 @@
+package net.usases.kordsmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KordsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
